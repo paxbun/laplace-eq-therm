0ad562b43ec4bb18f31b5ce54fc2b5218caed465
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-10-12 08:24:39 +0900

    Add print_usage in client/src/main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,26 @@
+// Copyright (c) 2021 Chanjung Kim. All rights reserved.
+// Licensed under the MIT License.
+
+pub mod serial;
+
+fn print_usage() {
+    eprintln!("Usage: laplace-eq-therm-client <Arduino Serial port> <Server URL>");
+    eprintln!();
+    let devices = serial::Arduino::get_available_ports();
+    eprintln!(
+        "{} Arduino {} found",
+        devices.len(),
+        if devices.len() == 1 {
+            "device"
+        } else {
+            "devices"
+        }
+    );
+    for device in devices {
+        eprintln!(" * {}", device);
+    }
+}
+
 fn main() {
-    println!("Hello, world!")
+    print_usage();
 }
