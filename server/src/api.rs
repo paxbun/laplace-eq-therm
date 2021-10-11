06d7e407e84f205bf83c8d094f415fadcb079534
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-10-12 07:58:39 +0900

    Add complete implementation of all API endpoints

--- server/src/api.rs
+++ server/src/api.rs
@@ -4,16 +4,42 @@
 //! Implements API endpoints.
 
 use super::server::Server;
-use laplace_eq_therm::{LocalInfo, SpaceInfo};
+use laplace_eq_therm::{LocalInfo, SimulationResult, SimulationResults};
 use rocket::State;
 
-#[get("/state")]
-pub fn get_state(server: State<Server>) -> SpaceInfo {
-    todo!();
+fn get_simulation_results(server: &Server) -> SimulationResults {
+    (0..server.get_num_spaces())
+        .map(|i| match server.get(i) {
+            Ok(result) => SimulationResult {
+                error_code: 0,
+                result,
+            },
+            Err(error_code) => SimulationResult {
+                error_code,
+                result: None,
+            },
+        })
+        .collect()
 }
 
+/// Returns the current simulation results.
+///
+/// # Arguments
+///
+/// * `server`: the server state
+#[get("/state")]
+pub fn get_state(server: State<Server>) -> SimulationResults {
+    get_simulation_results(&server)
+}
+
+/// Updates the temperature information at the given point and returns the current simulation results.
+///
+/// # Arguments
+///
+/// * `state`: new temperature information at the given point
+/// * `server`: the server state
 #[post("/state", data = "<state>")]
-pub fn update_state(state: LocalInfo, server: State<Server>) -> SpaceInfo {
+pub fn update_state(state: LocalInfo, server: State<Server>) -> SimulationResults {
     server.set(state.x, state.y, state.temp, state.r#type);
-    todo!();
+    get_simulation_results(&server)
 }
