10e7cb8f39621b67689d5a034e8cc8b1903d253d
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-10-12 09:11:02 +0900

    Reflect changes in server/src/server.rs

--- server/src/api.rs
+++ server/src/api.rs
@@ -4,24 +4,9 @@
 //! Implements API endpoints.
 
 use super::server::Server;
-use laplace_eq_therm::{LocalInfo, SimulationResult, SimulationResults};
+use laplace_eq_therm::{LocalInfo, SimulationResults};
 use rocket::State;
 use rocket_contrib::json::Json;
-
-fn get_simulation_results(server: &Server) -> SimulationResults {
-    (0..server.get_num_spaces())
-        .map(|i| match server.get(i) {
-            Ok(result) => SimulationResult {
-                error_code: 0,
-                result: Some(result),
-            },
-            Err(error_code) => SimulationResult {
-                error_code,
-                result: None,
-            },
-        })
-        .collect()
-}
 
 /// Returns the current simulation results.
 ///
@@ -30,7 +15,7 @@ fn get_simulation_results(server: &Server) -> SimulationResults {
 /// * `server`: the server state
 #[get("/state")]
 pub fn get_state(server: State<Server>) -> Json<SimulationResults> {
-    Json(get_simulation_results(&server))
+    Json(server.get_simulation_results())
 }
 
 /// Updates the temperature information at the given point and returns the current simulation results.
@@ -42,5 +27,5 @@ pub fn get_state(server: State<Server>) -> Json<SimulationResults> {
 #[post("/state", data = "<state>")]
 pub fn update_state(state: Json<LocalInfo>, server: State<Server>) -> Json<SimulationResults> {
     server.set(state.x, state.y, state.temp, state.r#type);
-    Json(get_simulation_results(&server))
+    Json(server.get_simulation_results())
 }
