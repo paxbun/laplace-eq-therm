b1bbb032a38d90b698b8894abd81f5aa48aa3f74
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-10-25 23:10:16 +0900

    Add sample unit test

---
+++ server/core/Tests/FooTest.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Chanjung Kim. All rights reserved.
+// Licensed under the MIT License.
+
+#include <gtest/gtest.h>
+#include <leth/Server.hh>
+
+TEST(FooTest, IsThreePlusFourSeven)
+{
+    ASSERT_EQ(3 + 4, 7);
+}
